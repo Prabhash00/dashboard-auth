1b5d60b4d1470d83dbe163c0b7d1e8840e27bc71
Prabhash Singh <prabhash.amazon.lko@gmail.com>
2025-05-01 13:57:02 +0530

    Additional Features

--- src/dashboardPages/components/sidebarComponents/ProfilePage.jsx
+++ src/dashboardPages/components/sidebarComponents/ProfilePage.jsx
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import ProfileCard from "../ProfileCard";
 
 function ProfilePage() {
   const navigate = useNavigate();
+  const [age, setAge] = useState(null);
+
+  const calculateAge = (dob) => {
+    const birthDate = new Date(dob);
+    const today = new Date();
+
+    let calculatedAge = today.getFullYear() - birthDate.getFullYear();
+    const monthDiff = today.getMonth() - birthDate.getMonth();
+    const dayDiff = today.getDate() - birthDate.getDate();
+
+    if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
+      calculatedAge--;
+    }
+    return calculatedAge;
+  };
+
+  const handleAge = (e) => {
+const dob=e.target.value;
+if (dob) {
+  const calculatedAge=calculateAge(dob)
+  setAge(calculatedAge)
+} else {
+  setAge(null)
+}
+
+  };
+
+
+
   return (
     <div className="bg-white p-4 rounded-lg w-[400px]">
       <div className="header-weather flex justify-center items-center w-auto">
@@ -12,7 +41,7 @@ function ProfilePage() {
       <ProfileCard />
       <div className="flex items-center gap-2 mt-2">
         <label
-          className="font-semibold text-lg flex ml-[40px] whitespace-nowrap"
+          className="font-semibold text-[17px] flex ml-[40px] whitespace-nowrap"
           htmlFor="dob"
         >
           Date of Birth:
@@ -20,9 +49,18 @@ function ProfilePage() {
         <input
           id="dob"
           type="date"
+          max="2000-12-31"
           className="border border-gray-300 rounded px-2 py-1 text-sm"
+          onChange={handleAge}
         />
       </div>
+        <div className="mt-2 ">
+         
+          <p>
+            <strong>Age is: </strong>
+            {age !== null ? `${age} years old` : "Not Set"}
+          </p>
+        </div>
 
       <button
         className="button-global mt-4"
